9c0cb2ce8788d8c57ab1b3b1b881202813965b88
Sky <wadfx95174@gmail.com>
2020-11-04 22:19:54 +0800

    Full version 1
    Refactor code.
    Package the file.

--- generate_modbus_packet.py
+++ generate_modbus_packet.py
@@ -5,73 +5,64 @@ class Modbus_TCP(Packet):
     fields_desc = [
         XShortField("TransactionIdentifier", 0x0001),
         XShortField("ProtocolIdentifier", 0x0000),
-        ShortField("Length", None),
-        XByteField("UnitIdentifier", 0x01),
-
-        # BitFieldLenField("Byte Count", 2, 8),
-        # FieldListField("Register Value", [0x0000], ShortField('', 0x0000)),
-        # ShortField("register", int('0000', 16)),
-        # ShortField("Word Count", int('0003', 16))
+        ShortField("Length", 0x0000),
+        XByteField("UnitIdentifier", 0x01)
     ]
 
 class Modbus(Packet):
     name = "Modbus"
-    # fields_desc = [
-    #     XByteField("FunctionCode", 0x04),
-    #     BitFieldLenField("ByteCount", None, 8, count_of="RegisterValue", adjust=lambda pkt, x: x*2),
-    #     FieldListField("RegisterValue", [], ShortField('', 0x0000), count_from=lambda pkt: pkt.ByteCount)
-    # ]
     fields_desc = [
         XByteField("FunctionCode", 0x04),
-        BitFieldLenField("ByteCount", None, 8),
-        FieldListField("RegisterValue", [], ShortField('', 0x0000))
+        BitFieldLenField("ByteCount", 0x00, 8, count_of="RegisterValue", adjust=lambda pkt, x: x*2),
+        FieldListField("RegisterValue", [], ShortField('', 0x0000), count_from=lambda pkt: pkt.ByteCount)
     ]
+    # fields_desc = [
+    #     XByteField("FunctionCode", 0x04),
+    #     BitFieldLenField("ByteCount", None, 8),
+    #     FieldListField("RegisterValue", [], ShortField('', 0x0000))
+    # ]
 
 # set Ethernet fields
-# src_mac = "00:25:90:bb:b3:e6"
-# dst_mac = "00:90:e8:79:3e:9d"
-src_mac = "00:0d:e0:81:3c:a5"
-dst_mac = "f4:28:53:1c:49:8d"
+EthernetDict = {
+    # 'src_mac' : "00:25:90:bb:b3:e6",
+    # 'dst_mac' : "00:90:e8:79:3e:9d",
+    'src_mac' : "00:0d:e0:81:3c:a5",
+    'dst_mac' : "f4:28:53:1c:49:8d",
+}
 
 # set IP fields
-# src_ip = "172.16.100.12"
-# dst_ip = "172.16.100.46"
-src_ip = "172.16.100.100"
-dst_ip = "172.16.100.200"
-ihl = 5
-length = 55
-ttl = 60
-proto = 'tcp'
-ip_id = 8
+IPDict = {
+    # 'src_ip' : "172.16.100.12",
+    # 'dst_ip' : "172.16.100.46",
+    'src_ip' : "172.16.100.100",
+    'dst_ip' : "172.16.100.200",
+    'ihl' : 5,
+    'length' : 55,
+    'ttl' : 60,
+    'proto' : 'tcp',
+    'ip_id' : 8
+}
+
 
 # set TCP fields
-sport = 502
-dport = 5000
-seq = int('d519a1b7', 16)
-ack = int('02d762b3', 16)
-tcp_flags = 'PA'
-window = 4096
-dataofs = 5
-urgptr = 0
+TCPDict = {
+    'sport' : 502,
+    'dport' : 5000,
+    'seq' : int('d519a1b7', 16),
+    'ack' : int('02d762b3', 16),
+    'tcp_flags' : 'PA',
+    'window' : 4096,
+    'dataofs' : 5,
+    'urgptr' : 0
+}
 
-modbus_TCP = Modbus_TCP()
-modbus = Modbus()
-
-modbus.RegisterValue.append(0x14e7)
-modbus.RegisterValue.append(0x0a4f)
-modbus.RegisterValue.append(0x1f0e)
-modbus_TCP.Length = 0x0009
-modbus.ByteCount = 0x06
-
-def generatePacket():
-    return Ether(src=src_mac, dst=dst_mac)/\
-        IP(src=src_ip, dst=dst_ip, ihl=ihl, len=length, ttl=ttl, proto=proto, chksum=None)/\
-        TCP(sport=sport, dport=dport, flags=tcp_flags, window=window, dataofs=dataofs, chksum=None, urgptr=urgptr)/\
+def generatePacket(modbus_TCP, modbus):
+    return Ether(src=EthernetDict['src_mac'], dst=EthernetDict['dst_mac'])/\
+        IP(src=IPDict['src_ip'], dst=IPDict['dst_ip'], ihl=IPDict['ihl']\
+            , len=IPDict['length'], ttl=IPDict['ttl'], proto=IPDict['proto'], chksum=None)/\
+        TCP(sport=TCPDict['sport'], dport=TCPDict['dport'], flags=TCPDict['tcp_flags']\
+            , window=TCPDict['window'], dataofs=TCPDict['dataofs'], chksum=None, urgptr=TCPDict['urgptr'])/\
         modbus_TCP/modbus
 
-pkt = generatePacket()
-print(pkt.display())
-# print(ls(IP))
-# print(ls(TCP))
-# print(ls(Modbus))
-sendp(pkt)
+# print(pkt.display())
+# sendp(pkt)
