9f233b6d3b8928160198855408029c24af2b9000
Sky <wadfx95174@gmail.com>
2020-11-04 20:33:26 +0800

    Modify code
    Change various field parameters

--- generate_modbus_packet.py
+++ generate_modbus_packet.py
@@ -1,28 +1,31 @@
-from scapy impotr *
+from scapy.all import *
 
-class Modbus(Packet):
-    name = "Modbus/tcp"
+class Modbus_TCP(Packet):
+    name = "Modbus/TCP"
     fields_desc = [
-        # ShortField("Transaction Identifier", int('00001', 16)),
-        # ShortField("Protocol Identifier", int('0000', 16)),
-        # ShortField("Length", int('5', 16)),
-        ShortField("Transaction Identifier", 1),
-        ShortField("Protocol Identifier", 0),
-        ShortField("Length", 6),
-        XByteField("Unit Identifier", int('1', 16)),
-        XByteField("Function Code", int('4', 16)),
-        # BitFieldLenField("Byte Count", None, 6),
-        # FieldListField("Register Value", [], [5346, 2640, 7952]),
+        XShortField("TransactionIdentifier", 0x0001),
+        XShortField("ProtocolIdentifier", 0x0000),
+        ShortField("Length", None),
+        XByteField("UnitIdentifier", 0x01),
+
         # BitFieldLenField("Byte Count", 2, 8),
         # FieldListField("Register Value", [0x0000], ShortField('', 0x0000)),
-        ShortField("register", int('0000', 16)),
-        ShortField("Word Count", int('0003', 16))
+        # ShortField("register", int('0000', 16)),
+        # ShortField("Word Count", int('0003', 16))
     ]
+
+class Modbus(Packet):
+    name = "Modbus"
     # fields_desc = [
-    #     XByteField("Function Code", 0x04),
-    #     BitFieldLenField("byteCount", None, 8, count_of="registerVal", adjust=lambda x: x*2),
-    #     FieldListField("registerVal", [0x0000], ShortField('', 0x0000), count_from=lambda pkt: pkt.byteCount)
+    #     XByteField("FunctionCode", 0x04),
+    #     BitFieldLenField("ByteCount", None, 8, count_of="RegisterValue", adjust=lambda pkt, x: x*2),
+    #     FieldListField("RegisterValue", [], ShortField('', 0x0000), count_from=lambda pkt: pkt.ByteCount)
     # ]
+    fields_desc = [
+        XByteField("FunctionCode", 0x04),
+        BitFieldLenField("ByteCount", None, 8),
+        FieldListField("RegisterValue", [], ShortField('', 0x0000))
+    ]
 
 # set Ethernet fields
 # src_mac = "00:25:90:bb:b3:e6"
@@ -36,7 +39,7 @@ dst_mac = "f4:28:53:1c:49:8d"
 src_ip = "172.16.100.100"
 dst_ip = "172.16.100.200"
 ihl = 5
-length = 52
+length = 55
 ttl = 60
 proto = 'tcp'
 ip_id = 8
@@ -51,11 +54,20 @@ window = 4096
 dataofs = 5
 urgptr = 0
 
+modbus_TCP = Modbus_TCP()
+modbus = Modbus()
+
+modbus.RegisterValue.append(0x14e7)
+modbus.RegisterValue.append(0x0a4f)
+modbus.RegisterValue.append(0x1f0e)
+modbus_TCP.Length = 0x0009
+modbus.ByteCount = 0x06
+
 def generatePacket():
     return Ether(src=src_mac, dst=dst_mac)/\
         IP(src=src_ip, dst=dst_ip, ihl=ihl, len=length, ttl=ttl, proto=proto, chksum=None)/\
         TCP(sport=sport, dport=dport, flags=tcp_flags, window=window, dataofs=dataofs, chksum=None, urgptr=urgptr)/\
-        Modbus()
+        modbus_TCP/modbus
 
 pkt = generatePacket()
 print(pkt.display())
