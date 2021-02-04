ae474140447f84f022c670404f9664ba27e05dd5
wadfx95174 <wadfx95174@gmail.com>
2021-02-04 14:43:15 +0800

    C_area version

--- generate_modbus_packet.py
+++ generate_modbus_packet.py
@@ -6,7 +6,7 @@ class Modbus_TCP(Packet):
         XShortField("TransactionIdentifier", 0x0001),
         XShortField("ProtocolIdentifier", 0x0000),
         ShortField("Length", 0x0000),
-        XByteField("UnitIdentifier", 0x01)
+        XByteField("UnitIdentifier", 0x00)
     ]
 
 class Modbus(Packet):
@@ -21,21 +21,32 @@ class Modbus(Packet):
     #     BitFieldLenField("ByteCount", None, 8),
     #     FieldListField("RegisterValue", [], ShortField('', 0x0000))
     # ]
+class ModbusError(Packet):
+    name = "Exception"
+    fields_desc = [
+        XByteField("FunctionCode", 0x84),
+        ShortField("ExceptionCode", 0x06)
+    ]
+
 
 # set Ethernet fields
 EthernetDict = {
-    # 'src_mac' : "00:25:90:bb:b3:e6",
-    # 'dst_mac' : "00:90:e8:79:3e:9d",
-    'src_mac' : "00:0d:e0:81:3c:a5",
-    'dst_mac' : "f4:28:53:1c:49:8d",
+    'dst_mac' : "00:25:90:bb:b3:e6",
+    'src_mac' : "00:90:e8:79:3e:9d",
+    # 'src_mac' : "00:0d:e0:81:3c:a5",
+    # 'dst_mac' : "f4:28:53:1c:49:8d",
+    # 'src_mac': "dc:a6:32:91:53:d6",
+    # 'dst_mac': "ac:22:0b:8c:72:a2"
 }
 
 # set IP fields
 IPDict = {
-    # 'src_ip' : "172.16.100.12",
-    # 'dst_ip' : "172.16.100.46",
-    'src_ip' : "172.16.100.100",
-    'dst_ip' : "172.16.100.200",
+    'src_ip' : "172.16.100.12",
+    'dst_ip' : "172.16.100.46",
+    # 'src_ip' : "172.16.100.100",
+    # 'dst_ip' : "172.16.100.200",
+    # 'src_ip' : "192.168.2.6",
+    # 'dst_ip' : "192.168.2.2",
     'ihl' : 5,
     'length' : 55,
     'ttl' : 60,
@@ -48,8 +59,8 @@ IPDict = {
 TCPDict = {
     'sport' : 502,
     'dport' : 5000,
-    'seq' : int('d519a1b7', 16),
-    'ack' : int('02d762b3', 16),
+    'seq' : 0,
+    'ack' : 0,
     'tcp_flags' : 'PA',
     'window' : 4096,
     'dataofs' : 5,
@@ -60,8 +71,9 @@ def generatePacket(modbus_TCP, modbus):
     return Ether(src=EthernetDict['src_mac'], dst=EthernetDict['dst_mac'])/\
         IP(src=IPDict['src_ip'], dst=IPDict['dst_ip'], ihl=IPDict['ihl']\
             , len=IPDict['length'], ttl=IPDict['ttl'], proto=IPDict['proto'], chksum=None)/\
-        TCP(sport=TCPDict['sport'], dport=TCPDict['dport'], flags=TCPDict['tcp_flags']\
-            , window=TCPDict['window'], dataofs=TCPDict['dataofs'], chksum=None, urgptr=TCPDict['urgptr'])/\
+        TCP(sport=TCPDict['sport'], dport=TCPDict['dport'], seq=TCPDict['seq'], ack=TCPDict['ack']\
+            , flags=TCPDict['tcp_flags'], window=TCPDict['window'], dataofs=TCPDict['dataofs']\
+            , chksum=None, urgptr=TCPDict['urgptr'])/\
         modbus_TCP/modbus
 
 # print(pkt.display())
